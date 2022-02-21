7dde11d92748864d1b733a25c34cdac1930192a1
TauAkiou <karaken@gmail.com>
2022-02-21 10:57:48 -0500

    initial autotracking support

--- scripts/init.lua
+++ scripts/init.lua
@@ -18,5 +18,9 @@ else if (string.find(Tracker.ActiveVariantUID, "map_tracker"))
     Tracker:AddLayouts("layouts/broadcast_vertical.json")
     Tracker:AddLocations("locations/Crater.json")
     Tracker:AddLocations("locations/Cave_Networks.json")
+
+    if PopVersion and PopVersion>="0.18.0" then
+        ScriptHost:LoadScript("scripts/autotracking.lua")    
+    end
 end
 end
