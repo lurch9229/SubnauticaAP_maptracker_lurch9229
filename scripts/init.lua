535f338a13a66eb8e51045dcfa9d07f5fd6ae8a9
lurch9229 <38219473+lurch9229@users.noreply.github.com>
2022-07-26 10:25:32 +0100

    dev test for 1.3.0

--- scripts/init.lua
+++ scripts/init.lua
@@ -11,6 +11,10 @@ if (string.find(Tracker.ActiveVariantUID, "items_only"))
     then
     Tracker:AddLayouts("layouts/items_only.json")
     Tracker:AddLayouts("layouts/broadcast_vertical.json")
+    
+    if PopVersion and PopVersion>="0.18.0" then
+        ScriptHost:LoadScript("scripts/autotracking.lua")    
+    end
 else if (string.find(Tracker.ActiveVariantUID, "map_tracker")) 
     then
     Tracker:AddMaps("maps/maps.json")
@@ -18,5 +22,10 @@ else if (string.find(Tracker.ActiveVariantUID, "map_tracker"))
     Tracker:AddLayouts("layouts/broadcast_vertical.json")
     Tracker:AddLocations("locations/Crater.json")
     Tracker:AddLocations("locations/Cave_Networks.json")
+    Tracker:AddLocations("locations/Creatures.json")
+
+    if PopVersion and PopVersion>="0.18.0" then
+        ScriptHost:LoadScript("scripts/autotracking.lua")    
+    end
 end
 end
